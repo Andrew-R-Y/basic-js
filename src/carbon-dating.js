5c73175b94466206106327df5c481ca5d5868675
Andrew-R-Y <rakitinandrei@gmail.com>
2023-04-13 20:26:38 +0300

    feat: solve task_2 Carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,22 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (!sampleActivity) {return false};
+  let result;
+  if (Number.isNaN(sampleActivity * 1)) {
+    return false;
+  } 
+  if ((sampleActivity * 1) <= 0 || (sampleActivity * 1) > 15) {
+    return false;
+  } 
+  if (typeof(sampleActivity) === 'string') {
+    result = Math.ceil((5730 / 0.693) * Math.log(15 / (sampleActivity * 1)));
+    console.log(result);
+    return result;
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
