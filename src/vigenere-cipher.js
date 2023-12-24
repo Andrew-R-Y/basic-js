a925ffa7349160372ce37eedabf0c0ea306b300f
Andrew-R-Y <rakitinandrei@gmail.com>
2023-12-24 20:16:07 +0300

    feat: add partial solution of Vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,33 +3,105 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.direct = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(stringIncome, keyIncome) {
+    const baseAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+    if (!stringIncome || !keyIncome) {
+      throw new Error('Incorrect arguments!');
+    }
+    let string = stringIncome.toLowerCase();
+    let key = keyIncome.toLowerCase();
+    const keyArr = [];
+    for (let i = 0; i < key.length; i += 1) {
+      keyArr.push(baseAlphabet.search(key[i]));
+    }
+    let result = '';
+    let offset = 0;
+    for (let i = 0; i < string.length; i += 1) {
+      let nextLetter = '';
+      const regExp = /[a-z]/;
+      if (!regExp.test(string[i]) || baseAlphabet.search(string[i]) < 0) {
+        result += string[i];
+        offset += 1;
+      } else {
+        nextLetter = baseAlphabet.charAt(
+          (baseAlphabet.search(string[i]) + keyArr[(i - offset) % key.length]) %
+            26
+        );
+        result += nextLetter;
+      }
+    }
+    if (!this.direct) {
+      const resArr = result.split('');
+      resArr.reverse();
+      result = resArr.join('');
+    }
+    return result.toUpperCase();
+  }
+
+  decrypt(stringUp, keyIncome) {
+    const baseAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+    if (!stringUp || !keyIncome) {
+      throw new Error('Incorrect arguments!');
+    }
+    let string = stringUp.toLowerCase();
+    let key = keyIncome.toLowerCase();
+    if (!this.direct) {
+      const incomeArr = string.split('');
+      incomeArr.reverse();
+      string = incomeArr.join('');
+    }
+    const keyArr = [];
+    for (let i = 0; i < key.length; i += 1) {
+      keyArr.push(baseAlphabet.search(key[i]));
+    }
+    let result = '';
+    let offset = 0;
+    for (let i = 0; i < string.length; i += 1) {
+      let nextLetter = '';
+      const regExp = /[a-z]/;
+      if (!regExp.test(string[i]) || baseAlphabet.search(string[i]) < 0) {
+        result += string[i];
+        offset += 1;
+      } else {
+        nextLetter = baseAlphabet.charAt(
+          (26 +
+            baseAlphabet.search(string[i]) -
+            keyArr[(i - offset) % key.length]) %
+            26
+        );
+        result += nextLetter;
+      }
+    }
+    if (!this.direct) {
+      const resArr = result.split('');
+      resArr.reverse();
+      result = resArr.join('');
+    }
+    return result.toUpperCase();
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
--- src/what-season.js
+++ src/what-season.js
@@ -2,41 +2,54 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
 function getSeason(date) {
   if (!date) {
-    return 'Unable to determine the time of year!'
+    return 'Unable to determine the time of year!';
   }
   const fnWithError = () => {
-    throw new Error ('Invalid date!')
-  }
+    throw new Error('Invalid date!');
+  };
   if (isNaN(Date.parse(date))) {
-    fnWithError()
+    fnWithError();
   }
   if (typeof date !== 'object') {
-    fnWithError()
+    fnWithError();
   }
-  if ((date instanceof Date) !== true) {
-    fnWithError()
+  if (date instanceof Date !== true) {
+    fnWithError();
   }
-  if ((typeof date.getTimezoneOffset) !== 'function') {
+  if (typeof date.getTimezoneOffset !== 'function') {
     fnWithError();
   }
   const month = date.getMonth();
-  const seasons = ['winter', 'winter', 'spring', 'spring', 'spring', 'summer', 'summer', 'summer', 'autumn', 'autumn', 'autumn', 'winter'];
+  const seasons = [
+    'winter',
+    'winter',
+    'spring',
+    'spring',
+    'spring',
+    'summer',
+    'summer',
+    'summer',
+    'autumn',
+    'autumn',
+    'autumn',
+    'winter',
+  ];
   result = seasons[month];
-  console.log(date.getMonth())
+  console.log(date.getMonth());
   return result;
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
